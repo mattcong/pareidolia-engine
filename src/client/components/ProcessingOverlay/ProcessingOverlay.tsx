3b09a1cb3429c5955e43c48204d43a5f19770be3
Matt Congdon <mattcong@users.noreply.github.com>
2026-03-15 14:04:38 +0000

    feat: processing overlay

---
+++ src/client/components/ProcessingOverlay/ProcessingOverlay.tsx
@@ -0,0 +1,9 @@
+import "./ProcessingOverlay.css"
+
+export const ProcessingOverlay = () => {
+  return (
+    <div className="processing-overlay">
+      <span className="processing-overlay__label">detecting</span>
+    </div>
+  )
+}
